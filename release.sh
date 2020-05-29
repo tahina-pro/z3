e73b4be322995bb58b3ed9c3b424f445cdcdda86
Tahina Ramananandro <taramana@microsoft.com>
2020-05-29 01:36:00 -0700

    test

--- release.sh
+++ release.sh
@@ -9,8 +9,20 @@ docker container create --name=z3c z3
 mkdir z3
 docker cp z3c:/home/test/z3/build/z3 z3/
 docker container rm z3c
-tagname=$(git rev-parse --abbrev-ref HEAD | sed 's!^_!!')
+commit_hash=$(git show --no-patch --format=%h)
+if [[ -f tag ]] ; then
+    tagname=$(cat tag)
+    tagged_commit_hash=$(git show --no-patch --format=%h $tagname)
+    [[ "$tagged_commit_hash" = "$commit_hash" ]]
+else
+    branchname=$(git rev-parse --abbrev-ref HEAD)
+    tagname=$(echo $branchname | sed 's!^_!!')
+    echo $tagname > tag
+    git add tag
+    git commit -m "release $tagname"
+    git tag $tagname
+    git push --set-upstream origin $branchname
+    git push $tagname
+fi
 platform=$(uname -m)
-git tag "$tagname"
-git push origin "$tagname"
 tar czf $tagname-$platform.tar.gz z3/*
