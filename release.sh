8bd31452a179e3623b90bdb2e8bcdf06219a2edf
Tahina Ramananandro <taramana@microsoft.com>
2020-05-29 01:17:53 -0700

    create an archive

--- release.sh
+++ release.sh
@@ -6,8 +6,11 @@ if [[ -z "$BUILD_THREADS" ]] ; then
 fi
 docker build -t z3 --build-arg BUILD_THREADS="$BUILD_THREADS" .
 docker container create --name=z3c z3
-docker cp z3c:/home/test/z3/build/z3 .
-docker container remove z3c
+mkdir z3
+docker cp z3c:/home/test/z3/build/z3 z3/
+docker container rm z3c
 tagname=$(git rev-parse --abbrev-ref HEAD | sed 's!^_!!')
+platform=$(uname -m)
 git tag "$tagname"
 git push origin "$tagname"
+tar czf $tagname-$platform.tar.gz z3/*
